8661d27fbc658bda081038d444cf5c63a6e6a300
m-m-black <morganblack1988@gmail.com>
2018-10-06 15:53:06 +1000

    Final script for part 1

--- heart-for-javanns.sh
+++ heart-for-javanns.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
 
-# Total number of instance = 606
-TRAIN=303
-VALID=121
-TEST=181
-INPUT=10
-OUTPUT=10
+# Total number of instance = 592
+TRAIN=296
+VALID=118
+TEST=177
+INPUT=26
+OUTPUT=2
 
 LOCATION="/Users/mblack/Documents/RMIT/2018 Sem 2/DM/Assignment 2/Files"
 FILE="$LOCATION/heart-v1-norm.arff"
+
+# sed -e "s/\bTEXT\b/g"
+# anything between \b and \b will be treated as the value to search for
+
+# used sed -i '' '/?/d' heart-v1-norm.arff to remove lines with ?
 
 # Remove preamble, shuffle rows, send to temp1.txt
 tail -n +20 "$FILE" | 
 	fgrep -v "%" |
 	gshuf |
-	sed -e "s/,/ /g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/value/new_value/g" |
-	sed -e "s/<50/1 0/g" |
-	sed -e "s/>50_1/0 1/g" > "$LOCATION/temp1.txt"
+	sed -e "s/,/ /g" | 
+	sed -e "s/ male / 1 0 /g" |
+	sed -e "s/ female / 0 1 /g" |
+	sed -e "s/ typ_angina / 1 0 0 0 /g" |
+	sed -e "s/ asympt / 0 1 0 0 /g" |
+	sed -e "s/ non_anginal / 0 0 1 0 /g" |
+	sed -e "s/ atyp_angina / 0 0 0 1 /g" |
+	sed -e "s/ t / 1 0 /g" |
+	sed -e "s/ f / 0 1 /g" | 
+	sed -e "s/ left_vent_hyper / 1 0 0 /g" |
+	sed -e "s/ normal / 0 1 0 /g" |
+	sed -e "s/ st_t_wave_abnormality / 0 0 1 /g" |
+	sed -e "s/ no / 1 0 /g" |
+	sed -e "s/ yes / 0 1 /g" |
+	sed -e "s/ down / 1 0 0 /g" |
+	sed -e "s/ flat / 0 1 0 /g" |
+	sed -e "s/ up / 0 0 1 /g" |
+	sed -e "s/ fixed_defect / 1 0 0 /g" |
+	sed -e "s/ normal / 0 1 0 /g" |
+	sed -e "s/ reversable_defect / 0 0 1 /g" |
+	sed -e "s/ <50/ 1 0 /g" |
+	sed -e "s/ >50_1/ 0 1 /g" > "$LOCATION/temp1.txt"
 
 # Training file
 /bin/echo "SNNS pattern definition file V3.2"  > "$LOCATION/heart-train.pat"
@@ -53,7 +58,7 @@ head -$TRAIN "$LOCATION/temp1.txt" >> "$LOCATION/heart-train.pat"
 /bin/echo "generated at Mon Apr 25 15:58:23 1994"  >> "$LOCATION/heart-valid.pat"
 /bin/echo ""  >> "$LOCATION/heart-valid.pat"
 /bin/echo ""  >> "$LOCATION/heart-valid.pat"
-/bin/echo "No. of patterns : $TRAIN"  >> "$LOCATION/heart-valid.pat"
+/bin/echo "No. of patterns : $VALID"  >> "$LOCATION/heart-valid.pat"
 /bin/echo "No. of input units : $INPUT"  >> "$LOCATION/heart-valid.pat"
 /bin/echo "No. of output units : $OUTPUT"  >> "$LOCATION/heart-valid.pat"
 
@@ -65,7 +70,7 @@ tail -n +$FROM "$LOCATION/temp1.txt" | head -$VALID >> "$LOCATION/heart-valid.pa
 /bin/echo "generated at Mon Apr 25 15:58:23 1994"  >> "$LOCATION/heart-test.pat"
 /bin/echo ""  >> "$LOCATION/heart-test.pat"
 /bin/echo ""  >> "$LOCATION/heart-test.pat"
-/bin/echo "No. of patterns : $TRAIN"  >> "$LOCATION/heart-test.pat"
+/bin/echo "No. of patterns : $TEST"  >> "$LOCATION/heart-test.pat"
 /bin/echo "No. of input units : $INPUT"  >> "$LOCATION/heart-test.pat"
 /bin/echo "No. of output units : $OUTPUT"  >> "$LOCATION/heart-test.pat"
 
